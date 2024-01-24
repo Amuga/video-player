37552319fe236b26b14aed4b153b548adb7b177b
Amuga <43413033+Amuga@users.noreply.github.com>
2024-01-24 07:57:29 +0100

    Commit
    Lets go team

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
+import { mediaJSON } from './testData'
+import  playSvg  from './assets/play.svg'
+import  pauseSvg  from './assets/pause.svg'
+import forwardSvg from './assets/forward.svg'
+import backwardSvg from './assets/backward.svg'
+import nextSvg from './assets/next.svg'
+import previousSvg from './assets/previous.svg'
 import './App.css';
 
 function App() {
+  const [playlist, setPlaylist] = useState(mediaJSON.categories[0].videos)
+  const [currentMedia, setCurrentMedia] = useState(0) // Index of currently played media
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [urlInput, setUrlInput] = useState('')
+  const videoRef = useRef()
+
+  const loadMedia = () => {
+    const loadedMedia = playlist[currentMedia].sources[0];
+    videoRef.current.src = loadedMedia
+    videoRef.current.load()
+  }
+
+  useEffect(() => {
+    loadMedia()
+  }, [currentMedia])
+
+
+  const handleAddMedia = () => {
+    const newMedia = {
+      description: urlInput,
+      sources: [urlInput],
+      subtitle: "",
+      thumb: "",
+      title: urlInput,
+    };
+    setPlaylist([...playlist, newMedia])
+    setUrlInput('')
+  }
+  
+  const handleRemoveMedia = (index) => {
+    const updatedPlaylist = playlist.filter((_, i) => i !== index)
+    setPlaylist(updatedPlaylist)
+    /* known bug with removing the first video (if it's the one being played) and the player not updating
+      probably related with updating the index state to the same value (0 to 0) not triggering an update */
+    setCurrentMedia( index === currentMedia ? 0 : currentMedia)
+  }
+  
+  const handlePlayPause = () => {
+    
+    if (videoRef.current.paused) {
+        videoRef.current.play()
+        setIsPlaying(true) 
+    } else {
+        videoRef.current.pause()
+        setIsPlaying(false) 
+    }
+  }
+
+  const handleFastBackward = () => {
+    videoRef.current.currentTime -= 10
+  }
+
+  const handleFastForward = () => {
+    videoRef.current.currentTime += 10
+  }
+
+  const handlePreviousMedia = () => {
+    setIsPlaying(false)
+    if (currentMedia === 0) {
+      setCurrentMedia(playlist.length - 1)
+    } else {
+      setCurrentMedia(currentMedia - 1)
+    }
+  }
+  
+  const handleNextMedia = () => {
+    setIsPlaying(false)
+    if (currentMedia === playlist.length - 1) {
+      setCurrentMedia(0);
+    } else {
+      setCurrentMedia(currentMedia + 1)
+    }
+  }
+
+  const handleInputChange = e => {
+    setUrlInput(e.target.value)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Playlist</h2>
+      <ul>
+        {playlist.map((media, index) => (
+          <li
+            key={index}
+            style={{ fontWeight: index === currentMedia ? 'bold' : 'normal' }}
+          >
+            {media.title}
+            <button onClick={() => handleRemoveMedia(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      
+      <h2>Add Media</h2>
+      <input type="text" id="mediaUrl" onChange={handleInputChange} placeholder='Enter URL' value={urlInput}/>
+      <button onClick={handleAddMedia}>
+        Add
+      </button>
+      <div>
+        <video id="video" ref={videoRef}  onClick={handlePlayPause}/>
+      </div>
+      <button onClick={handlePreviousMedia}>
+        <img src={previousSvg} alt='Previous Media'/>
+      </button>
+      <button onClick={handleFastBackward}>
+        <img src={backwardSvg}  alt='Fast Backward'/>
+      </button>
+      <button onClick={handlePlayPause}>
+        <img
+            src={isPlaying ? pauseSvg : playSvg}
+            alt={isPlaying ? 'Pause' : 'Play'}
+          />
+      </button>
+      <button onClick={handleFastForward}>
+        <img src={forwardSvg} alt='Fast Forward'/>
+      </button>
+      <button onClick={handleNextMedia}>
+        <img src={nextSvg} alt='Next Media'/>
+      </button>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/testData.js
@@ -0,0 +1,21 @@
+export const mediaJSON = { "categories" : [ { "name" : "Movies",
+        "videos" : [ 
+		    { "description" : "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
+              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" ],
+              "subtitle" : "By Blender Foundation",
+              "thumb" : "images/BigBuckBunny.jpg",
+              "title" : "Big Buck Bunny"
+            },
+            { "description" : "The first Blender Open Movie from 2006",
+              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" ],
+              "subtitle" : "By Blender Foundation",
+              "thumb" : "images/ElephantsDream.jpg",
+              "title" : "Elephant Dream"
+            },
+            { "description" : "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org", 
+            "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4" ],
+            "subtitle" : "By Blender Foundation",
+            "thumb" : "images/TearsOfSteel.jpg",
+            "title" : "Tears of Steel"
+            }, 
+    ]}]};
